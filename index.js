12dd335bc2cc2ec35cd1b971e3230ce96ccd40f3
Benjamin Ta <benjaminta@berkeley.edu>
2017-11-12 19:55:54 -0800

    lots of stuff

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-
-/*
-
-function prompt(){
-var person = prompt("Please enter your name", "Harry Potter");
-
-if (person == null || person == "") {
-    txt = "User cancelled the prompt.";
-} else {
-    txt = "Hello " + person + "! How are you today?";
-}
-document.getElementById("demo").innerHTML = txt;
-*/
 function myFunction() {
     var txt;
-    var person = prompt("Please enter your name:", "Harry Potter");
+    var person = prompt("Please enter your latitude:", "37");
     if (person == null || person == "") {
         txt = "User cancelled the prompt.";
     } else {
-        txt = "Hello " + person + "! How are you today?";
+        txt = "Your latitude is " + person + " degrees north.";
+        /*
+        var longitude = prompt("Please enter your longitude", "-122");
+        if (latitude == null || person == "") {
+            txt = "User cancelled the prompt.";
+        } else {
+            txt = txt + "The coordinates are " + person + " north and " + longitude + "east."
+        }
+        */
     }
     document.getElementById("demo").innerHTML = txt;
 }
+
+
+function onClick() {
+    var json;
+    function reqListener () {
+        //console.log(this.responseText);
+        json = JSON.parse(this.responseText);
+        var test;
+        test = json[0].neighbourhood_cleansed;
+        console.log(test);
+    }
+
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", "https://api.myjson.com/bins/v6wo3");
+    oReq.send();
+}
+
+ 
